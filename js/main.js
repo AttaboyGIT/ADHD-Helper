df3bc1ae2b22522c744d164c00b35e011322a6df
Jakub Sroda <jakob.sroda@gmail.com>
2025-05-18 18:31:10 +0200

    reset

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-document.querySelector('#createList').addEventListener('click', createList)
+document.querySelector('#createList').addEventListener('click', createList);
 
+const activeListView = [];
+
+localStorage.setItem('activeListView', JSON.stringify(activeListView)); // save activeListView to local storage
 
 function createList() {
-    /*
-    get the name & description
-    create object
-    */
-    alert('click works');
+    const listName = document.querySelector('#getListName').value;
+    const listDescription = document.querySelector('#getListDescription').value;
+
+    const todoList = {
+        name: listName,
+        description: listDescription,
+        items: [],
+        removeList: function(){/*remove this object*/},
+        renameList: function(newName) { this.name = newName; }
+    };
+
+    // Use the properties directly, not this.name/this.description
+    todoList.listUI = `<div class="list">
+        <h2>${todoList.name}</h2>
+        <p>${todoList.description}</p>
+        <button id="removeList">Remove List</button>
+        <button id="renameList">Rename List</button>
+    </div>`;
+
+    document.querySelector('#listView').innerHTML = todoList.listUI;
+    activeListView.push(todoList);
+
+    console.log(todoList);
 }
+
+
+
